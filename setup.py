94a3530a3ff931fd6784a19fd0e508558b9bd73d
Rajesh Singh <rajeshrinet@gmail.com>
2020-06-04 17:43:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,48 @@
-import numpy as np
-import os, sys 
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-import Cython.Compiler.Options
-Cython.Compiler.Options.annotate = True
-
-setup(
-    name='PyRossGeo',
-    version='1.0.0',
-    url='https://github.com/lukastk/PyRossGeo.git',
-    author='The PyRossGeo team',
-    license='MIT',
-    description='python library for spatial numerical simulation of infectious diseases',
-    long_description='pyrossgeo is a library for spatially resolved mathematical modelling of infectious diseases',
-    platforms='works on all platforms (such as LINUX, macOS, and Microsoft Windows)',
-    ext_modules=cythonize([ Extension('pyrossgeo/*', ["pyrossgeo/*.pyx"],
-        include_dirs=[np.get_include()],
-        ), Extension('pyrossgeo.mft', ["pyrossgeo/mft/*.pyx"],
-        include_dirs=[np.get_include()],
-        )],
-        compiler_directives={"language_level": 3},#sys.version_info[0]},
-        ),
-    libraries=[],
-    packages=['pyrossgeo'],
-    package_data={'pyrossgeo': ['*.pxd']},
-)
+import numpy as np                                                                                                    
+import os, sys                                                                                                        
+from distutils.core import setup                                                                                      
+from Cython.Build import cythonize                                                                                    
+from distutils.extension import Extension                                                                             
+import Cython.Compiler.Options                                                                                        
+Cython.Compiler.Options.annotate = True                                                                               
+                                                                                                                      
+                                                                                                                      
+if 'darwin'==(sys.platform).lower():                                                                                  
+    extension1 = Extension('pyross/*', ['pyrossgeo/*.pyx'],                                                           
+        include_dirs=[numpy.get_include()],                                                                           
+        extra_compile_args=['-mmacosx-version-min=10.9'],                                                             
+        extra_link_args=['-mmacosx-version-min=10.9'],                                                                
+    )                                                                                                                 
+else:                                                                                                                 
+    extension1 = Extension('pyross/*', ['pyrossgeo/*.pyx'],                                                           
+        include_dirs=[numpy.get_include()],                                                                           
+    )                                                                                                                 
+                                                                                                                      
+if 'darwin'==(sys.platform).lower():                                                                                  
+    extension2 = Extension('pyross/*', ['pyrossgeo/mft/*.pyx'],                                                       
+        include_dirs=[numpy.get_include()],                                                                           
+        extra_compile_args=['-mmacosx-version-min=10.9'],                                                             
+        extra_link_args=['-mmacosx-version-min=10.9'],                                                                
+    )                                                                                                                 
+else:                                                                                                                 
+    extension2 = Extension('pyross/*', ['pyross/mft/*.pyx'],                                                          
+        include_dirs=[numpy.get_include()],                                                                           
+    )                                                                                                                 
+                                                                                                                      
+                                                                                                                      
+setup(                                                                                                                
+    name='PyRossGeo',                                                                                                 
+    version='1.0.0',                                                                                                  
+    url='https://github.com/lukastk/PyRossGeo.git',                                                                   
+    author='The PyRossGeo team',                                                                                      
+    license='MIT',                                                                                                    
+    description='python library for spatial numerical simulation of infectious diseases',                             
+    long_description='pyrossgeo is a library for spatially resolved mathematical modelling of infectious diseases',   
+    platforms='works on all platforms (such as LINUX, macOS, and Microsoft Windows)',                                 
+    ext_modules=cythonize([extension1, extension2],                                                                   
+        compiler_directives={"language_level": 3},#sys.version_info[0]},                                              
+        ),                                                                                                            
+    libraries=[],                                                                                                     
+    packages=['pyrossgeo'],                                                                                           
+    package_data={'pyrossgeo': ['*.pxd']},                                                                            
+)                                                                                                                     
