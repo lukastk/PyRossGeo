d46518128f88a52d044982dcc8a5644ddb9785b7
Rajesh Singh <rajeshrinet@gmail.com>
2020-06-04 17:51:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import numpy as np                                                                                                    
+import numpy numpy                                                                                                    
 import os, sys                                                                                                        
 from distutils.core import setup                                                                                      
 from Cython.Build import cythonize                                                                                    
